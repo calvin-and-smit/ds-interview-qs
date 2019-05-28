832d04699130d9dd4596ea009712daafa19c8818
Smit Mehta <mehta.smit92@gmail.com>
2019-05-28 10:23:37 -0400

    Update cleaning-data.py

--- python/cleaning-data.py
+++ python/cleaning-data.py
@@ -1,11 +1,11 @@
 '''
 Below is a snippet from a table that contains information about employees that work at Company XYZ:
 
-employee_name	employee_id	date_joined	age	yrs_of_experience
-Andy	        123456	    2015-02-15	45	24
-Beth	        789456	    NaN	36	15
-Cindy	        654123	    2017-05-16	34	14
-Dale	        963852	    2018-01-15	25	4
+employee_name	  employee_id	  date_joined	  age	  yrs_of_experience
+Andy	          123456	      2015-02-15	  45	  24
+Beth	          789456	      NaN	          36	  15
+Cindy	          654123	      2017-05-16	  34	  14
+Dale	          963852	      2018-01-15	  25	  4
 
 Company XYZ recently migrated database systems causing some of the date_joined records to be NULL. 
 You're told by an analyst in human resources NULL records for the date_joined field indicates the employees joined prior to 2010. 
