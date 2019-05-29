e03987af4b593dc389420751a402db6665ac6330
Smit Mehta <mehta.smit92@gmail.com>
2019-05-29 17:01:57 -0400

    Update popular_songs.sql

--- sql/popular_songs.sql
+++ sql/popular_songs.sql
@@ -2,17 +2,17 @@
 
 Below are two table schemas for a popular music streaming application:
 Table 1: user_song_log
-Column Name	Data Type	Description
-user_id	id	id of the streaming user
-timestamp	integer	timestamp of when the user started listening to the song, epoch seconds
-song_id	integer	id of the song
-artist_id	integer	id of the artist
+Column Name	  Data Type	  Description
+user_id	      id	        id of the streaming user
+timestamp	    integer	    timestamp of when the user started listening to the song, epoch seconds
+song_id	      integer	    id of the song
+artist_id	    integer	    id of the artist
 
 Table 2: song_info
-Column Name	Data Type	Description
-song_id	integer	id of the song
-artist_id	integer	id of the artist
-song_length	integer	length of song in seconds
+Column Name	  Data Type	  Description
+song_id	      integer	    id of the song
+artist_id	    integer	    id of the artist
+song_length	  integer	    length of song in seconds
 
 
 Question: 
