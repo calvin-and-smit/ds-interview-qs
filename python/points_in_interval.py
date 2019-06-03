0e72905d082515aa8ef0989ec53af78871d2969c
Smit Mehta <mehta.smit92@gmail.com>
2019-06-03 14:37:27 -0400

    Create points_in_interval.py

---
+++ python/points_in_interval.py
@@ -0,0 +1,10 @@
+
+'''
+Question 16 - Points within an interval
+________________________________________
+You are given j integer intervals in the format [a,b] (e.g. if j = 2, would get list of 2 intervals [a,b], [c,d] 
+where a, b, c, and d are integer #s). The absolute value of the coordinates is bound by a value M. 
+
+Build a Python function that determines whether or not a given point belongs to the maximum number of intervals provided. 
+'''
+
