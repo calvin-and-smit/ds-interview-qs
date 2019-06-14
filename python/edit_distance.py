00bff14011cb390e93656106ce78cb1ccb56122f
Smit Mehta <mehta.smit92@gmail.com>
2019-06-14 11:28:12 -0400

    Create edit_distance.py

---
+++ python/edit_distance.py
@@ -0,0 +1,15 @@
+'''
+Question 20 - One edit away
+________________________________________
+Write a function to return a boolean that indicates if two strings are one edit away from being identical. The function should take in 2 strings and return a boolean. The definition of an "edit" is as follows: 
+•  Insert one character 
+•  Remove one character 
+•  Replace one character 
+A few examples of inputs and the function result are listed below.
+
+•  OneEditAway("pea", "peas") = True
+•  OneEditAway("pea", "fleas") = False
+•  OneEditAway("pea", "lea") = True
+•  OneEditAway("pea", "seas") = False
+'''
+
