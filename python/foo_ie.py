b691376d84daf0c0675df8c45dc81632db2510e5
Smit Mehta <mehta.smit92@gmail.com>
2019-06-24 13:42:35 -0400

    Update foo_ie.py

--- python/foo_ie.py
+++ python/foo_ie.py
@@ -11,4 +11,12 @@ Write a function that takes in an integer n, and prints out integers from 1 to n
 
 
 def foo(n):
-  return n
+    for i in range(1, n+1):
+        if i % 15 == 0:
+            print('foo-ie')
+        elif i % 5 == 0:
+            print('ie')
+        elif i % 3 == 0:
+            print('foo')
+        else:
+            print(i)
