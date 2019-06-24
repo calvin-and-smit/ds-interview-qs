e125902492b6113bca160ba7772db3a4f7aee162
Smit Mehta <mehta.smit92@gmail.com>
2019-06-24 13:36:01 -0400

    Create foo_ie.py

---
+++ python/foo_ie.py
@@ -0,0 +1,11 @@
+
+"""
+Question 24 - Oh foo-ie!
+________________________________________
+Write a function that takes in an integer n, and prints out integers from 1 to n inclusive. 
+- If %3 == 0 then print "foo" in place of the integer, 
+- if %5 == 0 then print "ie" in place of the integer, and 
+- if both conditions are true then print "foo-ie" in place of the integer.
+
+"""
+
