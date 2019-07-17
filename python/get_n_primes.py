52ceb0546556889dfffebcabe4355aeca6a537d5
Smit Mehta <mehta.smit92@gmail.com>
2019-07-17 10:53:37 -0400

    Create get_n_primes.py

---
+++ python/get_n_primes.py
@@ -0,0 +1,5 @@
+"""
+Question 34 - Identifying prime numbers with Python
+________________________________________
+A prime number (or a prime) is a natural number greater than 1 that cannot be formed by multiplying two smaller natural numbers. Given a single #, n, write a function using Python to return whether or not the # is prime. Additionally, if the inputted # is prime, save it into an array, a. 
+"""
