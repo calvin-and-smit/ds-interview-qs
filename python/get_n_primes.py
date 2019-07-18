3719b45ff800eb0923f880b2529863e84a0d7758
Smit Mehta <mehta.smit92@gmail.com>
2019-07-18 11:05:24 -0400

    Update get_n_primes.py

--- python/get_n_primes.py
+++ python/get_n_primes.py
@@ -5,3 +5,17 @@ A prime number (or a prime) is a natural number greater than 1 that cannot be fo
 Given a single #, n, write a function using Python to return whether or not the # is prime. 
 Additionally, if the inputted # is prime, save it into an array, a. 
 """
+
+
+def check_prime(n):
+    
+    if n == 1:
+        print(str(n) + ' is not a prime number')
+        return
+    halfway = int(n/2) + 1
+    for i in range(2, halfway+1):
+        if n % i == 0:
+            print(str(n) + ' is not a prime number')
+            return
+    print(str(n) + ' is a prime number')
+    return
