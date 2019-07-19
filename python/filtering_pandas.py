8a94847a3a9b36dc9f4a904697e78b83c179682c
Smit Mehta <mehta.smit92@gmail.com>
2019-07-19 11:03:19 -0400

    Create filtering_pandas.py

---
+++ python/filtering_pandas.py
@@ -0,0 +1,16 @@
+"""
+
+Question 35 - Filtering student information with Pandas
+________________________________________
+Suppose you have a dataframe, df, with the following records:
+
+Age	Favorite Color	Grade	Name
+20	blue	88	Willard Morris
+19	blue	95	Al Jennings
+22	yellow	92	Omar Mullins
+21	green	70	Spencer McDaniel
+The dataframe is showing information about a group of students. 
+Write code using Python Pandas to select the rows where the students' favorite color is blue or yellow and their grade is above 90. 
+
+"""
+
