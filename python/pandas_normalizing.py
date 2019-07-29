ae13f7f74c6e54567ec3ac08093150639087caa1
Smit Mehta <mehta.smit92@gmail.com>
2019-07-29 09:55:56 -0400

    Update pandas_normalizing.py

--- python/pandas_normalizing.py
+++ python/pandas_normalizing.py
@@ -1,10 +1,18 @@
+"""
+
 Question 39 - Normalizing student grades with Pandas
 ________________________________________
-You are given a dataframe containing student information, named df (shown below). Suppose you want to normalize each student's grade based on their age.
+You are given a dataframe containing student information, named df (shown below). 
+Suppose you want to normalize each student's grade based on their age.
 Age	Favorite Color	Grade	Name
 20	blue	88	Willard Morris
 19	blue	95	Al Jennings
 22	yellow	92	Omar Mullins
 21	green	70	Spencer McDaniel
 
-Write a function using Python Pandas that will add a new column to your dataframe containing a new grade normalized against the mean age of the students.
+Write a function using Python Pandas that will add a new column to your dataframe containing a new grade normalized against 
+the mean age of the students.
+
+"""
+
+import pandas as pd
