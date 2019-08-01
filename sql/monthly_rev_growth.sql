9a935f034f502af4a95af724f1ccd410fb3097b7
Smit Mehta <mehta.smit92@gmail.com>
2019-08-01 09:27:24 -0400

    Update monthly_rev_growth.sql

--- sql/monthly_rev_growth.sql
+++ sql/monthly_rev_growth.sql
@@ -5,7 +5,7 @@ ________________________________________
 Given the table below, called 'orders', write a SQL query to show the monthly revenue growth. 
 To calculate the monthly revenue growth, you can apply the following logic: 
 revenue growth = (current month's revenue-prior month's revenue)/prior month's revenue.
-order_id  channel	    date	      month	  revenue
+order_id    channel	    date	      month	  revenue
 1	        online	    2018-09-01	09	    100
 2	        online	    2018-09-03	09	    125
 3	        in_store	  2018-10-11	10	    200
