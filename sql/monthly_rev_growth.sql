589de071b385f46091c792c21785f622c7af92af
Smit Mehta <mehta.smit92@gmail.com>
2019-08-01 09:28:36 -0400

    Update monthly_rev_growth.sql

--- sql/monthly_rev_growth.sql
+++ sql/monthly_rev_growth.sql
@@ -8,8 +8,8 @@ revenue growth = (current month's revenue-prior month's revenue)/prior month's r
 order_id        channel	    date	      month	  revenue
 1	        online	    2018-09-01	09	    100
 2	        online	    2018-09-03	09	    125
-3	        in_store	2018-10-11	10	    200
-4	        in_store	2018-08-21 	08	    80
+3	        in_store  2018-10-11	10	    200
+4	        in_store  2018-08-21 	08	    80
 5	        online	    2018-08-13	08	    200
 
 ********************************************************************************************/
