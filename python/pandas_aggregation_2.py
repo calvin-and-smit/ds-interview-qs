8266b68bc419e417531b5d5da3eba5dac6fed724
Smit Mehta <mehta.smit92@gmail.com>
2019-08-07 10:26:10 -0400

    Update pandas_aggregation_2.py

--- python/pandas_aggregation_2.py
+++ python/pandas_aggregation_2.py
@@ -4,12 +4,12 @@ Question 43 - Revenue trends for online vs. in store channels
 ________________________________________
 Given the table below, called 'orders', write code to show the average revenue by month by channel: 
 
-order_id	  channel	  date	      month	  revenue
-1	          online	  2018-09-01	09	    100
-2	          online	  2018-09-03	09	    125
-3	          in_store	2018-10-11	10	    200
-4	          in_store	2018-08-21 	08	    80
-5	          online	  2018-08-13	08	    200
+order_id	  channel	  date	        month	  revenue
+1	          online	  2018-09-01	  09	    100
+2	          online	  2018-09-03	  09	    125
+3	          in_store	  2018-10-11	10	    200
+4	          in_store	  2018-08-21 	08	    80
+5	          online	  2018-08-13	  08	    200
 
 Your result should return the following in a structured table:
 Month | Channel | Avg. Revenue 
