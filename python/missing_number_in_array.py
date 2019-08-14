e516ef63bc4eeeea6bd454faf38dc3350ff400ba
Smit Mehta <mehta.smit92@gmail.com>
2019-08-14 10:06:37 -0400

    Update missing_number_in_array.py

--- python/missing_number_in_array.py
+++ python/missing_number_in_array.py
@@ -1,7 +1,11 @@
+"""
+
 Question 46 - Smallest missing number in array
 ________________________________________
-Write a function that outputs the smallest missing number in a sorted array of n unique integers. The integers in the array range from 0 to m-1, where m > n.
-The function should be called SmallestMissingNumber and the 3 inputs are: the array, the "start value" of the array (e.g. 0), and the length of the array - 1, or "end value". 
+Write a function that outputs the smallest missing number in a sorted array of n unique integers. 
+The integers in the array range from 0 to m-1, where m > n.
+The function should be called SmallestMissingNumber and the 3 inputs are: the array, the "start value" of the array (e.g. 0), 
+and the length of the array - 1, or "end value". 
 
 Examples: 
 Input: [0, 1, 3, 4, 8, 9], n = 5, m = 10 
@@ -9,3 +13,5 @@ Output: 2
 
 Input: [4, 7, 9, 11], n = 4, m = 12 
 Output: 0 
+
+"""
