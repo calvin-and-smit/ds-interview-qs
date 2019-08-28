318a4f67317187405bcb0c7220e9d4c730284b3f
Smit Mehta <mehta.smit92@gmail.com>
2019-08-28 11:21:30 -0400

    Update fradulent_retail_accounts.py

--- python/fradulent_retail_accounts.py
+++ python/fradulent_retail_accounts.py
@@ -7,8 +7,8 @@ Below is a daily table for an active accounts at Shopify (an online ecommerce, r
 The table is called store_account and the columns are:
 Column Name	  Data Type	  Description
 store_id	   integer	    a unique Shopify store id
-date	      string	    date
-status	        string	    Possible values are: [‘open’, 'closed’, ‘fraud’]
+date	          string	    date
+status	          string	    Possible values are: [‘open’, 'closed’, ‘fraud’]
 revenue	          double	    Amount of spend in USD
 Here's some more information about the table:
 
