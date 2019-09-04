b5d1661f8ceef694ded6e157ff6263a93debcd2f
Smit Mehta <mehta.smit92@gmail.com>
2019-09-04 09:52:03 -0400

    Create pandas_aggregation3.py

---
+++ python/pandas_aggregation3.py
@@ -0,0 +1,10 @@
+Active users on a messaging application
+
+Here is a table schema for a P2P messaging application. The table contains send/receive message data for the application's users. The structure is as follows:
+
+Table name: user_messaging
+
+date
+sender_id (#id of the message sender)
+receiver_id (#id of the message receiver)
+Question: Using Python, calculate what fraction of senders sent messages to at least 9 unique people on March 1, 2018.
