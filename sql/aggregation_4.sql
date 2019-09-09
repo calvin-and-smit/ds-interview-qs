1f58f71190143d9ac1386080860f7060f0dfa639
Smit Mehta <mehta.smit92@gmail.com>
2019-09-09 10:22:04 -0400

    Update aggregation_4.sql

--- sql/aggregation_4.sql
+++ sql/aggregation_4.sql
@@ -7,11 +7,11 @@ The survey logs data into a table called survey_logging. The schema of the table
 
 
 Column Name	Data Type	Description
-employee_id	integer	      employee id of the survey respondant
-action	        string	    Will be one of the following values, 'view', 'answer', 'skip'
-question_id	integer	      ID of the question asked
-answer_id	integer	      ID of the answer asked
-timestamp	integer	      time stamp of the action made by respondant
+employee_id	integer	        employee id of the survey respondant
+action	        string	        Will be one of the following values, 'view', 'answer', 'skip'
+question_id	integer	        ID of the question asked
+answer_id	integer	        ID of the answer asked
+timestamp	integer	        time stamp of the action made by respondant
 
 Question: Using SQL, find which question has the highest response rate.
 
