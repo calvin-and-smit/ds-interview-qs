681353bd039ba1d1db2e46ab1fa609521e157bbc
Smit Mehta <mehta.smit92@gmail.com>
2019-09-09 10:11:58 -0400

    Create power_sets.py

---
+++ python/power_sets.py
@@ -0,0 +1,13 @@
+'''
+Question 6 - Python function to express power sets
+
+Create a Python function that generates the power set given a set of values. 
+For example, if you're given the following set:
+
+set = {1, 2, 3}
+
+Your python function should return the corresponding power set:
+
+power set = { {}, {1}, {2}, {3}, {1, 2}, {2, 3}, {1, 3}, {1, 2, 3} }
+
+'''
