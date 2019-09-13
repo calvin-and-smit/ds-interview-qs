566be6870bb1ee13430a7799893e5689c77983b2
Smit Mehta <mehta.smit92@gmail.com>
2019-09-13 11:12:22 -0400

    Create gcd.py

---
+++ python/gcd.py
@@ -0,0 +1,16 @@
+Question 8 - Checking whether elements in array can be made equal through multiplication
+
+Given an array arr[], write a function to feed in the array elements check whether they can all be made equal by only multiplying the numbers by 2 or 7. (you can multiply by these #s as many times as you like)
+
+If all elements can be made equal, return False, otherwise return True.
+
+Example:
+
+arr[ ] = [128, 4, 2]
+
+128, 4*2*2*2*2*2 = 128, 2*2*2*2*2*2*2 = 128
+
+-- True
+arr[ ] = [65, 4, 2]
+
+-- False
