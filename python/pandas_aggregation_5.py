fb0018b3758c77c6cdbd3375a9c9eb4ad300bfa9
Smit Mehta <mehta.smit92@gmail.com>
2019-10-04 10:38:07 -0400

    Update pandas_aggregation_5.py

--- python/pandas_aggregation_5.py
+++ python/pandas_aggregation_5.py
@@ -5,11 +5,11 @@ Below is a snippet from a table that contains information about employees that w
 
 Column name	        Data type	Example value	  Description
 employee_name	        string	        Cindy	          Name of employee
-employee_id	        integer	        1837204	        Unique id for each employee
-yrs_of_experience	integer	        14	            total working years of experience
-yrs_at_company	        integer	        10	            total working years at Company XYZ
-compensation	        integer	        100000	        dollar value of employee compensation
-career_track	        string	        technical	      Potential values: technical, non-technical, executive
+employee_id	        integer	        1837204	          Unique id for each employee
+yrs_of_experience	integer	        14	          total working years of experience
+yrs_at_company	        integer	        10	              total working years at Company XYZ
+compensation	        integer	        100000	          dollar value of employee compensation
+career_track	        string	        technical	  Potential values: technical, non-technical, executive
 
 Company XYZ Human Resource department is trying to understand compensation across the company and asked 
 you to pull data to help them make a decision regarding employee compensation.
