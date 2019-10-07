f64e5a1165ebc69c2e541712b6ce639337ac03d7
Smit Mehta <mehta.smit92@gmail.com>
2019-10-07 10:46:50 -0400

    Update tinder_swipes.sql

--- sql/tinder_swipes.sql
+++ sql/tinder_swipes.sql
@@ -25,26 +25,26 @@ id	experiment	variant	  user_id
 
 `swipes`
 
-id	user_id	swiped_user_id	created_at	is_right_swipe
-1	123	893	      2018-01-01	0
-2	123	825	      2018-01-02	1
-3	567	946	      2018-01-04	0
-4	123	823	      2018-01-05	0
-5	567	952	      2018-01-05	1
-6	567	234	      2018-01-06	1
-7	996	333	      2018-01-06	1
-8	996	563	      2018-01-07	0
+id	user_id	swiped_user_id	created_at	  is_right_swipe
+1	123	893	        2018-01-01	0
+2	123	825	        2018-01-02	1
+3	567	946	        2018-01-04	0
+4	123	823	        2018-01-05	0
+5	567	952	        2018-01-05	1
+6	567	234	        2018-01-06	1
+7	996	333	        2018-01-06	1
+8	996	563	        2018-01-07	0
 Note: created_at doesn't show timestamps but assume it is a datetime column.
 
 Output:
 
-mean_right_swipes	variant	swipe_threshold	num_users
-5.3	control	10	9560
-5.6	test	10	9450
-20.1	control	50	2001
-22.0	test	50	2019
-33.0	control	100	590
-34.0	test	100	568
+mean_right_swipes	  variant	swipe_threshold	num_users
+5.3	      control	10	9560
+5.6	      test	10	9450
+20.1	    control	50	2001
+22.0	    test	50	2019
+33.0	    control	100	590
+34.0	    test	100	568
 
 
 *************************************************************************************************************/
