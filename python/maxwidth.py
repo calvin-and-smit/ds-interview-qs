a6a16c540282433723dfdca66bbb7873c567eeb4
Smit Mehta <mehta.smit92@gmail.com>
2019-10-25 10:25:40 -0400

    Update maxwidth.py

--- python/maxwidth.py
+++ python/maxwidth.py
@@ -1,19 +1,26 @@
-Max Width
-Good morning. Here's your algorithms question for today.
 
-This question was asked by: Microsoft
-Given an array of words and a maxWidth parameter, format the text such that each line has exactly maxWidth characters. Pad extra spaces ' ' when necessary so that each line has exactly maxWidth characters.
+#Max Width
+#Good morning. Here's your algorithms question for today.
 
-Extra spaces between words should be distributed as evenly as possible. If the number of spaces on a line do not divide evenly between words, the empty slots on the left will be assigned more spaces than the slots on the right.
+#This question was asked by: Microsoft
 
-Example:
+#Given an array of words and a maxWidth parameter, format the text such that each line has exactly maxWidth characters. 
+#Pad extra spaces ' ' when necessary so that each line has exactly maxWidth characters.
+
+#Extra spaces between words should be distributed as evenly as possible. If the number of spaces on a line do not 
+#divide evenly between words, the empty slots on the left will be assigned more spaces than the slots on the right.
+
+
+#Example:
 
 words = ["This", "is", "an", "example", "of", "text", "justification."]
 maxWidth = 16
 
+'''
 Output = 
 [
    "This    is    an",
    "example  of text",
    "justification.  "
 ]
+'''
