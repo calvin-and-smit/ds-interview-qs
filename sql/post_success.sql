90b41d5e4a1e3aff7166c65dbf1c93d65d2c856b
Smit Mehta <mehta.smit92@gmail.com>
2019-10-28 11:01:33 -0400

    Update post_success.sql

--- sql/post_success.sql
+++ sql/post_success.sql
@@ -6,8 +6,8 @@ This question was asked by: Facebook
 
 post_events 
 
-column	    type
-user_id	    int
+column	        type
+user_id	        int
 created_at	datetime
 event_name	varchar
 
@@ -20,9 +20,9 @@ Write a query to get the post success rate for each day over the past week.
 Sample:
 
 user_id	  created_at	event_name
-123	      2019-01-01	enter
-123	      2019-01-01	post
-456	      2019-01-02	enter
-456	      2019-01-02	cancel
+123	  2019-01-01	enter
+123	  2019-01-01	post
+456	  2019-01-02	enter
+456	  2019-01-02	cancel
 
 ******************************************************************************************************************/
