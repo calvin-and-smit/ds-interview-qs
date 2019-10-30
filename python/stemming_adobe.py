a5a38f7dfeb423aedcb1e997b2570250717d8067
Smit Mehta <mehta.smit92@gmail.com>
2019-10-30 09:27:34 -0400

    Update stemming_adobe.py

--- python/stemming_adobe.py
+++ python/stemming_adobe.py
@@ -1,11 +1,14 @@
+'''
 
 Question #20 | Replace words with stems
 Good morning. Here's your python question for today.
 
 This question was asked by: Adobe
-In data science, there exists the concept of stemming, which is the heuristic of chopping off the end of a word to clean and bucket it into an easier feature set. 
+In data science, there exists the concept of stemming, which is the heuristic of chopping off the end 
+of a word to clean and bucket it into an easier feature set. 
 
-Given a dictionary consisting of many roots and a sentence, stem all the words in the sentence with the root forming it. If a word has many roots can form it, replace it with the root with the shortest length.
+Given a dictionary consisting of many roots and a sentence, stem all the words in the sentence with the 
+root forming it. If a word has many roots can form it, replace it with the root with the shortest length.
 
 Example:
 
@@ -14,3 +17,5 @@ roots = ["cat", "bat", "rat"]
 sentence = "the cattle was rattled by the battery"
 
 Output: "the cat was rat by the bat"
+
+'''
