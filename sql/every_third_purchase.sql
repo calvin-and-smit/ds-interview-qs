f22f35678c2cae9d420cf48181677149d33d734e
Smit Mehta <mehta.smit92@gmail.com>
2019-11-11 09:21:40 -0500

    Update every_third_purchase.sql

--- sql/every_third_purchase.sql
+++ sql/every_third_purchase.sql
@@ -5,12 +5,12 @@ Third Purchase
 This question was asked by: Uber
 `transactions` table
 
-columns	type
-id	int
-user_id	int
-item	varchar
+columns	    type
+id	    int
+user_id	    int
+item	    varchar
 created_at	datetime
-revenue	float
+revenue	    float
 
 Given the revenue transaction table above that contains a user_id, created_at timestamp, and 
 transaction revenue, write a query that finds the third purchase of every user.
