6852029da64d18495d58d48b57b900568542791b
Smit Mehta <mehta.smit92@gmail.com>
2019-11-19 09:53:52 -0500

    Create one_element_removed.py

---
+++ python/one_element_removed.py
@@ -0,0 +1,12 @@
+'''
+Question #27 | One element removed
+
+This question was asked by: Facebook
+
+There are two lists, list X and list Y. Both lists contain integers from -1000 to 1000 and
+are identical to each other except that one integer is removed in list Y that exists in list X.
+
+Write a function that takes in both lists and returns the integer that was removed in 0(1) time 
+and O(n) space without using the python set function.
+
+'''
