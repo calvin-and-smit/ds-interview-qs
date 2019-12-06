8ebbe0e3770214ef470cee49795267361ce49b5f
Smit Mehta <mehta.smit92@gmail.com>
2019-12-06 11:19:29 -0500

    Create find_bigrams.py

---
+++ python/find_bigrams.py
@@ -0,0 +1,27 @@
+'''
+Find Bigrams
+
+
+This question was asked by: Indeed
+Write a function that can take a string and return a list of bigrams.
+
+Example:
+
+sentence = "Have free hours and love children? Drive kids to school, soccer practice and other activities."
+
+output = [('have', 'free'),
+ ('free', 'hours'),
+ ('hours', 'and'),
+ ('and', 'love'),
+ ('love', 'children?'),
+ ('children?', 'drive'),
+ ('drive', 'kids'),
+ ('kids', 'to'),
+ ('to', 'school,'),
+ ('school,', 'soccer'),
+ ('soccer', 'practice'),
+ ('practice', 'and'),
+ ('and', 'other'),
+ ('other', 'activities.')]
+ 
+ '''
