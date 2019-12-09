dbb91e494c7a8b7f7a1ae22e37cabac7c0491ec7
Smit Mehta <mehta.smit92@gmail.com>
2019-12-09 15:23:25 -0500

    Update and rename fiar_coin_from_biased_coin.py to fair_coin_from_biased_coin.py

---
+++ stats/fair_coin_from_biased_coin.py
@@ -0,0 +1,10 @@
+'''
+
+Biased random number generator
+
+
+This question was asked by: Amazon
+Given an unfair coin with the probability of heads and tails not equal to 50/50, 
+what algorithm could generate a list of random ones and zeros?
+
+'''
--- stats/fiar_coin_from_biased_coin.py
+++
@@ -1,5 +0,0 @@
-Biased random number generator
-Good morning. Here's your probability question for today.
-
-This question was asked by: Amazon
-Given an unfair coin with the probability of heads and tails not equal to 50/50, what algorithm could generate a list of random ones and zeros?
