a4b53089f7752b95ed162a73a3e05fb1a9aac8fa
Smit Mehta <mehta.smit92@gmail.com>
2019-12-12 11:04:25 -0500

    Update employee_salaries.sql

--- sql/employee_salaries.sql
+++ sql/employee_salaries.sql
@@ -1,7 +1,10 @@
+/************************************************************************************************
+
 Employee Salaries
-Good morning. Here's your sql question for today.
+
 
 This question was asked by: Microsoft
+
 employees table
 
 columns	types
@@ -19,7 +22,8 @@ id	int
 name	varchar
  
 
-1. Given the tables above, select the top 3 departments by the highest percentage of employees making over 100K in salary and have at least 10 employees.
+1. Given the tables above, select the top 3 departments by the highest percentage of employees 
+making over 100K in salary and have at least 10 employees.
 
 Example output:
 
@@ -27,6 +31,11 @@ Example output:
 90%	engineering	25
 50%	marketing	50
 12%	sales	12
-2. Let’s say due to an ETL error, the employee table instead of updating the salaries every year when doing compensation adjustments, did an insert instead. The head of HR still needs the current salary of each employee. Write a query to get the current salary for each employee.
+2. Let’s say due to an ETL error, the employee table instead of updating the salaries every year when 
+doing compensation adjustments, did an insert instead. The head of HR still needs the current salary of 
+each employee. Write a query to get the current salary for each employee.
 
 Assume no duplicate combination of first and last names. (I.E. No two John Smiths)
+
+
+************************************************************************************************/
