a48452d4750bcb65219d6735104625bb9140c0b5
Smit Mehta <mehta.smit92@gmail.com>
2019-12-30 10:25:38 -0500

    Create string_subsequence.py

---
+++ python/string_subsequence.py
@@ -0,0 +1,22 @@
+'''
+String Subsequence
+
+This question was asked by: Ike
+
+Given two strings, string1 and string2, find out if string1 is a subsequence of string2.
+
+A subsequence is a sequence that can be derived from another sequence by deleting some elements without 
+changing the order of the remaining elements.
+
+Example:
+
+string1 = 'abc'
+string2 = 'asbsc'
+string3 = 'acedb'
+
+isSubSequence(string1, string2) -> True
+isSubSequence(string1, string3) -> False
+
+'''
+
+
