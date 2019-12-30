f2150a96141ce6a41e7305f66a30c347b105242c
Smit Mehta <mehta.smit92@gmail.com>
2019-12-30 10:26:27 -0500

    Update string_subsequence.py

--- python/string_subsequence.py
+++ python/string_subsequence.py
@@ -20,3 +20,28 @@ isSubSequence(string1, string3) -> False
 '''
 
 
+# Refer this: https://www.geeksforgeeks.org/given-two-strings-find-first-string-subsequence-second/
+
+# Returns true if str1[] is a subsequence of str2[]. m is 
+# length of str1 and n is length of str2 
+def isSubSequence(string1, string2, m, n): 
+    # Base Cases 
+    if m == 0:    return True
+    if n == 0:    return False
+  
+    # If last characters of two strings are matching 
+    if string1[m-1] == string2[n-1]: 
+        return isSubSequence(string1, string2, m-1, n-1) 
+  
+    # If last characters are not matching 
+    return isSubSequence(string1, string2, m, n-1) 
+  
+# Driver program to test the above function 
+string1 = "gksrek"
+string2 = "geeksforgeeks"
+m = len(string1) 
+n = len(string2) 
+if isSubSequence(string1, string2, m, n): 
+    print "Yes"
+else: 
+    print "No"
