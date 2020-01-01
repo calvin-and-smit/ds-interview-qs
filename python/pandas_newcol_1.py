ba2c9d889f2664aa7de4e21a8e2f855a3f7b7f0a
Smit Mehta <mehta.smit92@gmail.com>
2020-01-01 13:21:51 -0500

    Create pandas_newcol_1.py

---
+++ python/pandas_newcol_1.py
@@ -0,0 +1,19 @@
+'''
+Categorizing foods
+
+You are given the following dataframe and are asked to categorize each food 
+into 1 of 3 categories: meat, fruit, or other.
+
+  food	pounds
+0	bacon	4.0
+1	STRAWBERRIES	3.5
+2	Bacon	7.0
+3	STRAWBERRIES	3.0
+4	BACON	6.0
+5	strawberries	9.0
+6	Strawberries	1.0
+7	pecans	3.0
+Given this, write code to add a new column categorizing each row.
+
+'''
+
