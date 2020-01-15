12eb9f0550954b4685da2c145cfd8d5a2ce8dffe
Smit Mehta <mehta.smit92@gmail.com>
2020-01-15 10:57:06 -0500

    Create string_shift.py

---
+++ python/string_shift.py
@@ -0,0 +1,17 @@
+'''
+Interview Query Question #53 | String Shift
+
+This question was asked by: Google
+Given two strings A and B, return whether or not A can be shifted some number of times to get B.
+
+Example:
+
+A = 'abcde'
+B = 'cdeab'
+can_shift(A, B) == True
+
+A = 'abc'
+B = 'acb'
+can_shift(A, B) == False
+
+'''
