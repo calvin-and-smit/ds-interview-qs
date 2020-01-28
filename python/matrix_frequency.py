f8f421f12dfa40e3a5b47ccfb2ebc8cd428e2959
Smit Mehta <mehta.smit92@gmail.com>
2020-01-28 12:31:10 -0500

    Create matrix_frequency.py

---
+++ python/matrix_frequency.py
@@ -0,0 +1,14 @@
+Matrix frequencies
+Python Algorithms Matrix
+Given a matrix of order m*n, find the frequency of even and odd numbers in the matrix.
+
+For example:
+
+#Given the following inputs
+m = 2, n = 3
+[[ 9, 11, 3 ], 
+[ 4, 12, 2 ]]
+
+#Your function should return the following output:
+#Frequency of odd #:  3 (since 9, 11, and 3 are all odd #s)
+#Frequency of even #: 3
