93e26878273e4de0d7e53143c40874aa25a24dfe
Smit Mehta <mehta.smit92@gmail.com>
2020-02-12 10:46:31 -0500

    Create string_mapping.py

---
+++ python/string_mapping.py
@@ -0,0 +1,25 @@
+'''
+Interview Query Question #64 | String mapping
+
+
+This question was asked by: IBM
+
+Given two strings, string1 and string, determine if there exists a one to one character mapping 
+between each character of string1 to string2.
+
+Example 1:
+
+string1 = 'qwe'
+string2 = 'asd'
+
+string_map(string1, string2) == True
+#q = a, w = s, and e = d
+Example 2:
+
+string1 = 'donut'
+string2 = 'fatty'
+
+string_map(string1, string2) == False
+#t cannot map to two different values
+
+'''
