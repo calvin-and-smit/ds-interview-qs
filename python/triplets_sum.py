03bdeacf987987bd93d1d315ab462e529741b488
Smit Mehta <mehta.smit92@gmail.com>
2020-03-19 11:52:42 -0400

    Create triplets_sum.py

---
+++ python/triplets_sum.py
@@ -0,0 +1,21 @@
+'''
+Count of triplets
+
+Given an array of unique integers and a sum value, can you write code to find the number of 
+triplets with a sum smaller than the given value?
+
+For example:
+
+#Given the following
+Array = [5, 1, 3, 4, 7]
+Sum value = 12
+
+#Output: 
+4
+
+#Explanation: 
+#There are 4 triplets that have a sum less than 12: 
+#(1, 3, 4), (1, 3, 5), (1, 3, 7), and (1, 4, 5)
+
+'''
+
