bbe11a1d81abe75d5d3a7d9ec3b16d1c76c3c912
Smit Mehta <mehta.smit92@gmail.com>
2020-04-01 20:26:39 -0400

    Create airbnb_marketing_channel.py

---
+++ python/airbnb_marketing_channel.py
@@ -0,0 +1,23 @@
+'''
+Marketing attribution
+
+Suppose you’re working at a company similar to Airbnb, and the marketing team has come to you to help them 
+figure out which channels (excluding direct) are best for them to focus on. You are given a dataset, which 
+can be found (https://docs.google.com/spreadsheets/d/1uNa9tpo8AEeJ9va_PU5AoIj430Iu4VFkML6twCyYy2A/)
+
+Below are the column descriptions for the dataset.
+
+column	description
+id	unique user identifier
+date_account_created	the date of account creation
+gender	user's gender
+age	user's age
+signup_method	how the user initially signed up
+language	user's language preference
+affiliate_channel	type of paid marketing that is attributed to user sign up
+affiliate_provider	provider of the paid "affiliate channel"
+signup_app	application that user signed up on
+first_device_type	first device user signed up with
+first_browser	first browser user signed up with
+
+Using the dataset above, can you help them make a recommendation to the team re: the most effective marketing channels?
