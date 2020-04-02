bfb6948d8dbab9904ed19b6ee65c88e7f5dc3a1c
Smit Mehta <mehta.smit92@gmail.com>
2020-04-01 21:16:03 -0400

    Create reverse_string.py

---
+++ python/reverse_string.py
@@ -0,0 +1,14 @@
+'''
+Reverse it
+
+Suppose you're given an array of strings, s. Write a function using Python to take each string in the array and reverse it.
+
+For example:
+
+#Given the following:
+s = ['rac', 'talf', 'tot', 'tob']
+
+#Your function should return:
+s = ['car', 'flat', 'tot', 'bot']
+
+'''
