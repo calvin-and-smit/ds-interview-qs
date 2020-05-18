9d5d071e53249bb91598a7386d646351afd346e0
Smit Mehta <mehta.smit92@gmail.com>
2020-05-18 10:34:46 -0400

    Create move_list_elements_to_end.py

---
+++ python/move_list_elements_to_end.py
@@ -0,0 +1,15 @@
+'''
+
+Given an array of size n, write a function to move x elements from the front to the end of the array.
+
+For example:
+
+#Given the following:
+array = [2,4,6,8,10]
+x = 3
+
+#To shift 3 elements you would return:
+[8,10,2,4,6]
+
+'''
+
