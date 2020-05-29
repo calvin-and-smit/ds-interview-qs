330ceaea05274b93689ac1460a1a47e494960975
Smit Mehta <mehta.smit92@gmail.com>
2020-05-28 22:45:15 -0400

    Create common_characters.py

---
+++ python/common_characters.py
@@ -0,0 +1,20 @@
+'''
+
+Print common characters using Python
+Python String Manipulation
+
+Given two strings, write code to print overlapping letters in alphabetical order.
+
+For example:
+
+#Given the following strings:
+str1 = 'card'
+str2 = 'shark'
+
+#Your overlapping letters would be 'a' (1x) and 'r' (1x), 
+#and they just happen to be in alphabetical order already, 
+#so your code would return:
+'ar'
+If there is no overlap between the two strings, you can print 'no overlap'.
+
+'''
