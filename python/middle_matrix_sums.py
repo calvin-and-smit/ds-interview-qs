7fd7e1b12a0b1bb932ecd2185e4c66513c50da95
Smit Mehta <mehta.smit92@gmail.com>
2020-06-05 13:22:46 -0400

    Create middle_matrix_sums.py

---
+++ python/middle_matrix_sums.py
@@ -0,0 +1,19 @@
+'''
+
+Middle matrix sums
+
+Given an integer matrix, m, with an odd # dimensions, n x n, (e.g 3 x 3, 5 x 5, etc), 
+find the sum of middle row as well as the middle column.
+
+For example:
+
+#Given
+[[1,2,3],
+[4,5,6],
+[7,8,9]]
+
+#Your program would output:
+'Sum middle row =' 15 #(e.g. 4+5+6)
+'Sum middle column =' 15 #(e.g. 2+5+8)
+
+'''
