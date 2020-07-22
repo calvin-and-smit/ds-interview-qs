93f7320860c35f77d6806fd3ca86c37647e05573
Smit Mehta <mehta.smit92@gmail.com>
2020-07-22 11:04:56 -0400

    Create subarray_sum.py

---
+++ python/subarray_sum.py
@@ -0,0 +1,18 @@
+'''
+
+Given an array of integers and a number X, can you find the smallest subarray with sum greater than the 
+given value? If there isn't a possible subarray, print "Not possible".
+
+For example:
+
+arr[] = [1, 4, 45, 6, 0, 19]
+x  =  51
+Output: 3
+# the array is [4, 45, 6]
+
+arr[] = [1, 10, 5, 2, 7]
+x  = 9
+Output: 1
+# the array is [10]
+
+'''
