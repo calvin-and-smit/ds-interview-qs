cbe4922f74ac0a4a43fa0426992e3841418b0962
Smit Mehta <mehta.smit92@gmail.com>
2020-08-26 16:01:45 -0400

    Create 00_linkedin_01.py

---
+++ python/00_linkedin_01.py
@@ -0,0 +1,15 @@
+'''
+
+This question was asked by: LinkedIn
+Given n sorted lists, create a combined list while maintaining sorted order.
+
+Example:
+
+A = [1,2,3,4,5,6]
+B = [2,5,7,8]
+C = [3,9,10,12]
+D = [0,1,2,8]
+
+output -> [0,1,1,2,2,3,3,4,5,5,6,7,8,8,9,10,12]
+
+'''
