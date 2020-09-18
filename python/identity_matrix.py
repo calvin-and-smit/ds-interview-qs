69c89ff6184a11507aa009e4539266f5b5ace817
Smit Mehta <mehta.smit92@gmail.com>
2020-09-18 13:04:58 -0400

    Create identity_matrix.py

---
+++ python/identity_matrix.py
@@ -0,0 +1,21 @@
+'''
+
+Can you build a function that prints an identity matrix, given the identity matrix size?
+
+Examples:
+
+Input: 2
+Output: 
+       1 0
+       0 1
+         
+
+Input: 4
+Output: 
+       1 0 0 0
+       0 1 0 0
+       0 0 1 0
+       0 0 0 1
+       
+
+'''
