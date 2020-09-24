ddc066fd872ee017bfab317d1a0d7f31ecc92f51
Smit Mehta <mehta.smit92@gmail.com>
2020-09-24 10:56:20 -0400

    Create 00_amazon_07.py

---
+++ python/00_amazon_07.py
@@ -0,0 +1,11 @@
+'''
+
+This question was asked by: Amazon
+Write a function that outputs the variance given a list of integers. 
+
+Example:
+
+test_list = [6, 7, 3, 9, 10, 15]
+get_variance(test_list) -> 13.888888888888891
+
+'''
