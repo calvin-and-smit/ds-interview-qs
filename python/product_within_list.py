1522263ab67a406cca03d1b179ff809859083f0e
Smit Mehta <mehta.smit92@gmail.com>
2020-10-07 11:50:31 -0400

    Create product_within_list.py

---
+++ python/product_within_list.py
@@ -0,0 +1,18 @@
+'''
+
+Suppose you're given an array of n elements. Within this array, return the greatest value of a 
+single element that is the product of two elements contained within the same array. If no such 
+product exists, print N/A. Lastly, you can assume that the values within the array are positive integers.
+
+For example:
+
+Input: array = [2, 2, 4, 5, 9] 
+Output: 4 #(since 2 * 2 = 4)
+
+Input: array = [1, 3, 4, 7, 10] 
+Output: 3 #(since 1 * 3 = 3)
+
+Input: array = [3, 3, 4, 7, 10] 
+Output: N/A
+
+'''
