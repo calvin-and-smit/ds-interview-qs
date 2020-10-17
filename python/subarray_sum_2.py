df818a70e6ddc7ff489b99fecb172e7fa2bb10b1
Smit Mehta <mehta.smit92@gmail.com>
2020-10-17 13:24:11 -0400

    Create subarray_sum_2.py

---
+++ python/subarray_sum_2.py
@@ -0,0 +1,18 @@
+'''
+
+Suppose you have an unsorted array of integers. Can you find the number of subarrays that have a sum exactly to the given number p?
+
+Input: 
+array = [10, 2, -2, -20, 10]
+k = -10
+Output : 3
+#The following subarrays [10, 2, -2, -20], [2, -2, -20, 10], [-20, 10] have a sum exactly equal to -10
+                
+Input: 
+array = [9, 4, 20, 3, 10, 5]
+k = 33
+Output : 2
+#The following subarrays [9, 4, 20], [20, 3, 10] have a sum exactly equal to 33
+
+
+'''
