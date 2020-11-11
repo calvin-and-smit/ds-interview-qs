362643d6853eb7878e9879087665998f8fbaf461
Smit Mehta <mehta.smit92@gmail.com>
2020-11-11 13:36:19 -0500

    Create 00_square_02.py

---
+++ python/00_square_02.py
@@ -0,0 +1,13 @@
+'''
+This question was asked by: Square
+You have store credit of N dollars. However, you don't want to walk a long distance with heavy books, but you want to spend all of your store credit.
+
+Let's say we have a list of books in the format of tuples where the first value is the price and the second value is the weight of the book -> (price,weight).
+
+What combination allows you to spend all of your store credit while getting at least two books at the lowest weight? 
+
+N = 18
+books = [(17,8), (9,4), (18,5), (11,9), (1,2), (13,7), (7,5), (3,6), (10,8)]
+
+optimal_books(N, books) -> [(17,8),(1,2)]
+'''
