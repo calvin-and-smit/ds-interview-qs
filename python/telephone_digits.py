d66f8509f226ad54be56ceba07e1dad47dde513d
Smit Mehta <mehta.smit92@gmail.com>
2020-12-21 09:48:07 -0500

    Create telephone_digits.py

---
+++ python/telephone_digits.py
@@ -0,0 +1,35 @@
+'''
+
+Suppose you're given a portion of a phone number. Each digit corresponds to letters (as shown below). 
+Using python, write code to return all possible combinations the given number could represent.
+
+Telephone digits, for reference:
+
+
+
+
+For example:
+
+Input: "24"
+
+Output: 
+
+['a', 'g']
+
+['a', 'h']
+
+['a', 'i']
+
+['b', 'g']
+
+['b', 'h']
+
+['b', 'i']
+
+['c', 'g']
+
+['c', 'h']
+
+['c', 'i']
+
+'''
