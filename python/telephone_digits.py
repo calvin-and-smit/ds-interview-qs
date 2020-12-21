e21fc4901eff5e8392733a6d8ac0ca989da11418
Smit Mehta <mehta.smit92@gmail.com>
2020-12-21 09:50:38 -0500

    Update telephone_digits.py

--- python/telephone_digits.py
+++ python/telephone_digits.py
@@ -5,7 +5,11 @@ Using python, write code to return all possible combinations the given number co
 
 Telephone digits, for reference:
 
-
+| 1      | 2  ABC | 3 DEF  |
+|--------|--------|--------|
+| 4 GHI  | 5 JKL  | 6 MNO  |
+| 7 PQRS | 8 TUV  | 9 WXYZ |
+| *      | 0      | #      |
 
 
 For example:
