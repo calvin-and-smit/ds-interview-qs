b3bb2a1ec37ec96c95a1f7efcafcf6fabc535c97
Smit Mehta <mehta.smit92@gmail.com>
2021-05-04 12:23:36 -0400

    Create 00_flexport_01.py

---
+++ python/00_flexport_01.py
@@ -0,0 +1,10 @@
+'''
+
+This problem was asked by Flexport.
+
+Given a string s, rearrange the characters so that any two adjacent characters are not the same. If this is not possible, return null.
+
+For example, if s = yyz then return yzy. If s = yyy then return null.
+
+
+'''
