9e451bdce79db62ec081b43a40f1fd9caf667e4d
Smit Mehta <mehta.smit92@gmail.com>
2021-06-02 14:19:14 -0400

    Create 00_robinhood_01.py

---
+++ python/00_robinhood_01.py
@@ -0,0 +1,16 @@
+'''
+
+This problem was asked by Robinhood.
+
+Given an array of strings, group anagrams together.
+
+For example, given the following array:
+
+['eat', 'ate', 'apt', 'pat', 'tea', 'now']
+Return:
+
+[['eat', 'ate', 'tea'],
+ ['apt', 'pat'],
+ ['now']]
+ 
+ '''
